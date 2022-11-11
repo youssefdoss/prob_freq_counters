0642c82b48f8cec9da73ab5eba126d4a33b77b74
David Luo <davidkluo@gmail.com>
2022-11-11 15:22:08 -0800

    Initial commit

---
+++ can-construct-word.test.js
@@ -0,0 +1,25 @@
+"use strict";
+
+describe("canConstructWord", function () {
+  it("should return false for empty letters string", function () {
+    expect(canConstructWord("abcd", "")).toBe(false);
+  });
+
+  it("should return true for empty message", function () {
+    expect(canConstructWord("", "abc")).toBe(true);
+  });
+
+  it("should account for duplicates", function () {
+    expect(canConstructWord("aa", "abcd")).toBe(false);
+  });
+
+  it("should handle large cases", function () {
+    expect(canConstructWord(
+        "skbjjjvnnd",
+        "fdjlkjfeburevjvnfnsjckjncjdnchbechbadhsd")).toBe(true);
+  });
+
+  it("should return true if all letters contained", function () {
+    expect(canConstructWord("abc", "abcd")).toBe(true);
+  });
+});
