296396e6252fce9a2c5d97b1679fe8050293128a
David Luo <davidkluo@gmail.com>
2022-11-11 16:10:11 -0800

    Implement twoArrayObject

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,32 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+/**
+ * twoArrayObject: Create an object from provided arrays of keys and values
+ * Extra values will be ignored, and insufficient values will be filled with null
+ *
+ * keys: array
+ * values: array
+ *
+ * return: object
+ */
+
+/*
+PSEUDOCODE
+
+Create an empty object for us to store our key-value pairs
+
+Iterate through the indices of the keys array
+  At each index
+    Create a key-value pair in the object, where the key is equal to the value of the keys array at the index,
+    and the value is equal to the value of the values array at the index if it exists, otherwise null.
+
+Return object
+*/
+
+function twoArrayObject(keys, values) {
+  let objOfPairs = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    objOfPairs[keys[i]] = values[i] || null;
+  }
+
+  return objOfPairs;
 }
